5dee4ce8506c4cb46afee219f822fefe26a5d69e
duduxiong <xianglinzong_cd@keruyun.com>
2018-12-23 10:41:33 +0800

    implement annotations for scenes

--- src/scenes/FogExp2.js
+++ src/scenes/FogExp2.js
@@ -1,35 +1,34 @@
-import { Color } from '../math/Color.js';
+import { Color } from "../math/Color.js";
 
 /**
  * @author mrdoob / http://mrdoob.com/
  * @author alteredq / http://alteredqualia.com/
  */
 
-function FogExp2( color, density ) {
+/**
+ * 具有指数性质的雾化效果
+ * @param {*} color
+ * @param {*} density
+ */
+function FogExp2(color, density) {
+  this.name = "";
 
-	this.name = '';
-
-	this.color = new Color( color );
-	this.density = ( density !== undefined ) ? density : 0.00025;
-
+  this.color = new Color(color);
+  this.density = density !== undefined ? density : 0.00025;
 }
 
 FogExp2.prototype.isFogExp2 = true;
 
-FogExp2.prototype.clone = function () {
-
-	return new FogExp2( this.color.getHex(), this.density );
-
+FogExp2.prototype.clone = function() {
+  return new FogExp2(this.color.getHex(), this.density);
 };
 
-FogExp2.prototype.toJSON = function ( /* meta */ ) {
-
-	return {
-		type: 'FogExp2',
-		color: this.color.getHex(),
-		density: this.density
-	};
-
+FogExp2.prototype.toJSON = function(/* meta */) {
+  return {
+    type: "FogExp2",
+    color: this.color.getHex(),
+    density: this.density
+  };
 };
 
 export { FogExp2 };
