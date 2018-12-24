825a4f164ec59236df0a8605b09f6e70259fee78
duduxiong <xianglinzong_cd@keruyun.com>
2018-12-24 15:19:41 +0800

    implement annotations for audio

--- src/audio/AudioContext.js
+++ src/audio/AudioContext.js
@@ -4,27 +4,19 @@
 
 var context;
 
-// 用于播放音频的上下文对象，该上下文对象一般有浏览器提供
+// 用于播放音频的上下文对象，该上下文对象一般由浏览器提供
 var AudioContext = {
-
-    getContext: function() {
-
-        if (context === undefined) {
-
-            context = new(window.AudioContext || window.webkitAudioContext)();
-
-        }
-
-        return context;
-
-    },
-
-    setContext: function(value) {
-
-        context = value;
-
+  getContext: function() {
+    if (context === undefined) {
+      context = new (window.AudioContext || window.webkitAudioContext)();
     }
 
+    return context;
+  },
+
+  setContext: function(value) {
+    context = value;
+  }
 };
 
 export { AudioContext };
--- src/audio/PositionalAudio.js
+++ src/audio/PositionalAudio.js
@@ -2,95 +2,70 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-import { Vector3 } from '../math/Vector3.js';
-import { Audio } from './Audio.js';
-import { Object3D } from '../core/Object3D.js';
+import { Vector3 } from "../math/Vector3.js";
+import { Audio } from "./Audio.js";
+import { Object3D } from "../core/Object3D.js";
 
 // 音频是声音在时间轴上的函数，因此需要位置相关的类
 function PositionalAudio(listener) {
+  Audio.call(this, listener);
 
-    Audio.call(this, listener);
-
-    this.panner = this.context.createPanner();
-    this.panner.connect(this.gain);
-
+  //   相当于变换时间轴来“快进”/“后退”音频信息
+  this.panner = this.context.createPanner();
+  this.panner.connect(this.gain);
 }
 
 PositionalAudio.prototype = Object.assign(Object.create(Audio.prototype), {
+  constructor: PositionalAudio,
 
-    constructor: PositionalAudio,
+  getOutput: function() {
+    return this.panner;
+  },
 
-    getOutput: function() {
+  getRefDistance: function() {
+    return this.panner.refDistance;
+  },
 
-        return this.panner;
+  setRefDistance: function(value) {
+    this.panner.refDistance = value;
+  },
 
-    },
+  getRolloffFactor: function() {
+    return this.panner.rolloffFactor;
+  },
 
-    getRefDistance: function() {
+  setRolloffFactor: function(value) {
+    this.panner.rolloffFactor = value;
+  },
 
-        return this.panner.refDistance;
+  getDistanceModel: function() {
+    return this.panner.distanceModel;
+  },
 
-    },
+  setDistanceModel: function(value) {
+    this.panner.distanceModel = value;
+  },
 
-    setRefDistance: function(value) {
+  getMaxDistance: function() {
+    return this.panner.maxDistance;
+  },
 
-        this.panner.refDistance = value;
+  setMaxDistance: function(value) {
+    this.panner.maxDistance = value;
+  },
 
-    },
+  //之所以需要更新音频对象在世界坐标系中的位置，其实就是为了模拟声音在空间中的传播
+  updateMatrixWorld: (function() {
+    var position = new Vector3();
 
-    getRolloffFactor: function() {
+    return function updateMatrixWorld(force) {
+      Object3D.prototype.updateMatrixWorld.call(this, force);
 
-        return this.panner.rolloffFactor;
+      position.setFromMatrixPosition(this.matrixWorld);
 
-    },
-
-    setRolloffFactor: function(value) {
-
-        this.panner.rolloffFactor = value;
-
-    },
-
-    getDistanceModel: function() {
-
-        return this.panner.distanceModel;
-
-    },
-
-    setDistanceModel: function(value) {
-
-        this.panner.distanceModel = value;
-
-    },
-
-    getMaxDistance: function() {
-
-        return this.panner.maxDistance;
-
-    },
-
-    setMaxDistance: function(value) {
-
-        this.panner.maxDistance = value;
-
-    },
-
-    updateMatrixWorld: (function() {
-
-        var position = new Vector3();
-
-        return function updateMatrixWorld(force) {
-
-            Object3D.prototype.updateMatrixWorld.call(this, force);
-
-            position.setFromMatrixPosition(this.matrixWorld);
-
-            this.panner.setPosition(position.x, position.y, position.z);
-
-        };
-
-    })()
-
-
+      this.panner.setPosition(position.x, position.y, position.z);
+    };
+  })()
 });
 
 export { PositionalAudio };
