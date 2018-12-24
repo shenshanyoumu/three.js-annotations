a63f7537566e2d3fac27a94e9742877f8acbf86f
duduxiong <xianglinzong_cd@keruyun.com>
2018-12-24 14:18:18 +0800

    implement annotations for extras

--- src/extras/core/Curve.js
+++ src/extras/core/Curve.js
@@ -1,425 +1,345 @@
-import { _Math } from '../../math/Math.js';
-import { Vector3 } from '../../math/Vector3.js';
-import { Matrix4 } from '../../math/Matrix4.js';
+import { _Math } from "../../math/Math.js";
+import { Vector3 } from "../../math/Vector3.js";
+import { Matrix4 } from "../../math/Matrix4.js";
 
 /**
- * @author zz85 / http://www.lab4games.net/zz85/blog
- * Extensible curve object
- *
- * Some common of curve methods:
- * .getPoint( t, optionalTarget ), .getTangent( t )
- * .getPointAt( u, optionalTarget ), .getTangentAt( u )
- * .getPoints(), .getSpacedPoints()
- * .getLength()
- * .updateArcLengths()
- *
- * This following curves inherit from THREE.Curve:
- *
- * -- 2D curves --
- * THREE.ArcCurve
- * THREE.CubicBezierCurve
- * THREE.EllipseCurve
- * THREE.LineCurve
- * THREE.QuadraticBezierCurve
- * THREE.SplineCurve
- *
- * -- 3D curves --
- * THREE.CatmullRomCurve3
- * THREE.CubicBezierCurve3
- * THREE.LineCurve3
- * THREE.QuadraticBezierCurve3
- *
- * A series of curves can be represented as a THREE.CurvePath.
- *
- **/
-
-/**************************************************************
- *	Abstract Curve base class
- **************************************************************/
-
+ * Curve基类，定义了曲线的构成和基础方法
+ */
 function Curve() {
+  this.type = "Curve";
 
-	this.type = 'Curve';
-
-	this.arcLengthDivisions = 200;
-
+  // 曲线积分长度分段，因为绘制曲线采样控制点方法插值
+  this.arcLengthDivisions = 200;
 }
 
-Object.assign( Curve.prototype, {
+Object.assign(Curve.prototype, {
+  // 具体的子类会重写该方法
+  getPoint: function() {
+    console.warn("THREE.Curve: .getPoint() not implemented.");
+    return null;
+  },
+
+  /**
+   * 得到t参数对应的曲线上的点
+   * @param {*} u 在[0,1]之间,用于映射到更加精细化的t参数
+   * @param {*} optionalTarget
+   */
+  getPointAt: function(u, optionalTarget) {
+    var t = this.getUtoTmapping(u);
+    return this.getPoint(t, optionalTarget);
+  },
+
+  /**
+   * 根据采样频度，得到采样点序列
+   * @param {*} divisions 曲线采样频度
+   */
+  getPoints: function(divisions) {
+    if (divisions === undefined) {
+      divisions = 5;
+    }
+
+    var points = [];
+
+    // 均匀采样
+    for (var d = 0; d <= divisions; d++) {
+      points.push(this.getPoint(d / divisions));
+    }
+
+    return points;
+  },
+
+  /**
+   * 根据采样频度并精细化采样点，而不是简单的均匀采样
+   * @param {*} divisions 对原生曲线的采样频度
+   */
+  getSpacedPoints: function(divisions) {
+    if (divisions === undefined) {
+      divisions = 5;
+    }
+
+    var points = [];
+
+    for (var d = 0; d <= divisions; d++) {
+      points.push(this.getPointAt(d / divisions));
+    }
+
+    return points;
+  },
+
+  // 计算整个曲线长度
+  getLength: function() {
+    var lengths = this.getLengths();
+    return lengths[lengths.length - 1];
+  },
+
+  // 下面返回的数组中，每个元素表示前面所有点形成的曲线的长度+当前点与前一个点的长度
+  // 因此数组表示曲线的分段累进长度
+  getLengths: function(divisions) {
+    if (divisions === undefined) {
+      divisions = this.arcLengthDivisions;
+    }
+
+    if (
+      this.cacheArcLengths &&
+      this.cacheArcLengths.length === divisions + 1 &&
+      !this.needsUpdate
+    ) {
+      return this.cacheArcLengths;
+    }
+
+    this.needsUpdate = false;
+
+    var cache = [];
+    var current,
+      last = this.getPoint(0);
+    var p,
+      sum = 0;
+
+    cache.push(0);
+
+    // 注意下面计算过程，表示曲线分段累进长度计算
+    for (p = 1; p <= divisions; p++) {
+      current = this.getPoint(p / divisions);
+      sum += current.distanceTo(last);
+      cache.push(sum);
+      last = current;
+    }
+
+    this.cacheArcLengths = cache;
+
+    return cache;
+  },
+
+  // 当曲线状态发生变化，触发更新操作
+  updateArcLengths: function() {
+    this.needsUpdate = true;
+    this.getLengths();
+  },
+
+  /**
+   * 给定参数u，计算对应的参数t并得到p点，从P点向两端等距distance采样
+   * @param {*} u 在[0,1]之间
+   * @param {*} distance 在曲线上采样点之间的等距长度
+   */
+  getUtoTmapping: function(u, distance) {
+    // 曲线的分段累进长度形成的数组
+    var arcLengths = this.getLengths();
+
+    var i = 0,
+      il = arcLengths.length;
+
+    var targetArcLength;
+
+    // 当显式传递distance，则设置targetArcLength为distance；否则基于曲线总长度在参数u处的值作为目标小弧线长度
+    if (distance) {
+      targetArcLength = distance;
+    } else {
+      targetArcLength = u * arcLengths[il - 1];
+    }
+
+    var low = 0,
+      high = il - 1,
+      comparison;
+
+    //二分查找，计算与targetArcLength最接近的原生曲线累进长度对应的索引点
+    while (low <= high) {
+      i = Math.floor(low + (high - low) / 2);
+
+      comparison = arcLengths[i] - targetArcLength;
+
+      if (comparison < 0) {
+        low = i + 1;
+      } else if (comparison > 0) {
+        high = i - 1;
+      } else {
+        high = i;
+        break;
+      }
+    }
+
+    i = high;
+
+    // 如果原生曲线在i点的累进长度恰好等于targetArcLength，则直接比例返回即可
+    if (arcLengths[i] === targetArcLength) {
+      return i / (il - 1);
+    }
+
+    // 如果原生曲线在i 点的累进长度只是最接近targetArcLength，则下面计算过程可以细粒度控制参数t的值
+    var lengthBefore = arcLengths[i];
+    var lengthAfter = arcLengths[i + 1];
+
+    // 计算原生曲线上i点和i+1点之间的小曲线长度
+    var segmentLength = lengthAfter - lengthBefore;
+
+    // targetArcLength - lengthBefore表示剩余量
+    var segmentFraction = (targetArcLength - lengthBefore) / segmentLength;
 
-	// Virtual base class method to overwrite and implement in subclasses
-	//	- t [0 .. 1]
+    var t = (i + segmentFraction) / (il - 1);
 
-	getPoint: function ( /* t, optionalTarget */ ) {
+    return t;
+  },
 
-		console.warn( 'THREE.Curve: .getPoint() not implemented.' );
-		return null;
+  // 计算在二维坐标系中，曲线参数t点对应的正切向量
+  getTangent: function(t) {
+    var delta = 0.0001;
+    var t1 = t - delta;
+    var t2 = t + delta;
 
-	},
+    // t的范围为[0，1]之间
+    if (t1 < 0) t1 = 0;
+    if (t2 > 1) t2 = 1;
 
-	// Get point at relative position in curve according to arc length
-	// - u [0 .. 1]
+    // 从参数t得到曲线在二维坐标系中的坐标点
+    var pt1 = this.getPoint(t1);
+    var pt2 = this.getPoint(t2);
 
-	getPointAt: function ( u, optionalTarget ) {
+    var vec = pt2.clone().sub(pt1);
+    return vec.normalize();
+  },
 
-		var t = this.getUtoTmapping( u );
-		return this.getPoint( t, optionalTarget );
+  /**
+   * 从u映射到t,用于得到精细控制的参数
+   * @param {*} u在[0,1]之间
+   */
+  getTangentAt: function(u) {
+    var t = this.getUtoTmapping(u);
+    return this.getTangent(t);
+  },
 
-	},
+  /**
+   * frenet坐标系，在无人驾驶领域，车辆与道路的关系用frenet坐标系描述比用大地坐标系的GPS位置描述更加清晰
+   * @param {*} segments
+   * @param {*} closed
+   */
 
-	// Get sequence of points using getPoint( t )
+  computeFrenetFrames: function(segments, closed) {
+    // see http://www.cs.indiana.edu/pub/techreports/TR425.pdf
 
-	getPoints: function ( divisions ) {
+    var normal = new Vector3();
 
-		if ( divisions === undefined ) divisions = 5;
+    var tangents = [];
+    var normals = [];
+    var binormals = [];
 
-		var points = [];
+    var vec = new Vector3();
+    var mat = new Matrix4();
 
-		for ( var d = 0; d <= divisions; d ++ ) {
-
-			points.push( this.getPoint( d / divisions ) );
-
-		}
-
-		return points;
-
-	},
-
-	// Get sequence of points using getPointAt( u )
-
-	getSpacedPoints: function ( divisions ) {
-
-		if ( divisions === undefined ) divisions = 5;
-
-		var points = [];
-
-		for ( var d = 0; d <= divisions; d ++ ) {
-
-			points.push( this.getPointAt( d / divisions ) );
-
-		}
-
-		return points;
-
-	},
-
-	// Get total curve arc length
-
-	getLength: function () {
-
-		var lengths = this.getLengths();
-		return lengths[ lengths.length - 1 ];
-
-	},
-
-	// Get list of cumulative segment lengths
-
-	getLengths: function ( divisions ) {
-
-		if ( divisions === undefined ) divisions = this.arcLengthDivisions;
-
-		if ( this.cacheArcLengths &&
-			( this.cacheArcLengths.length === divisions + 1 ) &&
-			! this.needsUpdate ) {
-
-			return this.cacheArcLengths;
-
-		}
-
-		this.needsUpdate = false;
-
-		var cache = [];
-		var current, last = this.getPoint( 0 );
-		var p, sum = 0;
-
-		cache.push( 0 );
-
-		for ( p = 1; p <= divisions; p ++ ) {
-
-			current = this.getPoint( p / divisions );
-			sum += current.distanceTo( last );
-			cache.push( sum );
-			last = current;
-
-		}
-
-		this.cacheArcLengths = cache;
-
-		return cache; // { sums: cache, sum: sum }; Sum is in the last element.
-
-	},
-
-	updateArcLengths: function () {
-
-		this.needsUpdate = true;
-		this.getLengths();
-
-	},
-
-	// Given u ( 0 .. 1 ), get a t to find p. This gives you points which are equidistant
-
-	getUtoTmapping: function ( u, distance ) {
-
-		var arcLengths = this.getLengths();
-
-		var i = 0, il = arcLengths.length;
-
-		var targetArcLength; // The targeted u distance value to get
-
-		if ( distance ) {
-
-			targetArcLength = distance;
-
-		} else {
-
-			targetArcLength = u * arcLengths[ il - 1 ];
-
-		}
-
-		// binary search for the index with largest value smaller than target u distance
-
-		var low = 0, high = il - 1, comparison;
-
-		while ( low <= high ) {
-
-			i = Math.floor( low + ( high - low ) / 2 ); // less likely to overflow, though probably not issue here, JS doesn't really have integers, all numbers are floats
-
-			comparison = arcLengths[ i ] - targetArcLength;
-
-			if ( comparison < 0 ) {
-
-				low = i + 1;
-
-			} else if ( comparison > 0 ) {
-
-				high = i - 1;
-
-			} else {
-
-				high = i;
-				break;
-
-				// DONE
-
-			}
-
-		}
-
-		i = high;
-
-		if ( arcLengths[ i ] === targetArcLength ) {
-
-			return i / ( il - 1 );
-
-		}
-
-		// we could get finer grain at lengths, or use simple interpolation between two points
-
-		var lengthBefore = arcLengths[ i ];
-		var lengthAfter = arcLengths[ i + 1 ];
-
-		var segmentLength = lengthAfter - lengthBefore;
-
-		// determine where we are between the 'before' and 'after' points
-
-		var segmentFraction = ( targetArcLength - lengthBefore ) / segmentLength;
-
-		// add that fractional amount to t
-
-		var t = ( i + segmentFraction ) / ( il - 1 );
-
-		return t;
-
-	},
-
-	// Returns a unit vector tangent at t
-	// In case any sub curve does not implement its tangent derivation,
-	// 2 points a small delta apart will be used to find its gradient
-	// which seems to give a reasonable approximation
-
-	getTangent: function ( t ) {
-
-		var delta = 0.0001;
-		var t1 = t - delta;
-		var t2 = t + delta;
-
-		// Capping in case of danger
-
-		if ( t1 < 0 ) t1 = 0;
-		if ( t2 > 1 ) t2 = 1;
-
-		var pt1 = this.getPoint( t1 );
-		var pt2 = this.getPoint( t2 );
-
-		var vec = pt2.clone().sub( pt1 );
-		return vec.normalize();
-
-	},
-
-	getTangentAt: function ( u ) {
-
-		var t = this.getUtoTmapping( u );
-		return this.getTangent( t );
-
-	},
-
-	computeFrenetFrames: function ( segments, closed ) {
-
-		// see http://www.cs.indiana.edu/pub/techreports/TR425.pdf
-
-		var normal = new Vector3();
-
-		var tangents = [];
-		var normals = [];
-		var binormals = [];
-
-		var vec = new Vector3();
-		var mat = new Matrix4();
-
-		var i, u, theta;
-
-		// compute the tangent vectors for each segment on the curve
-
-		for ( i = 0; i <= segments; i ++ ) {
-
-			u = i / segments;
-
-			tangents[ i ] = this.getTangentAt( u );
-			tangents[ i ].normalize();
-
-		}
-
-		// select an initial normal vector perpendicular to the first tangent vector,
-		// and in the direction of the minimum tangent xyz component
-
-		normals[ 0 ] = new Vector3();
-		binormals[ 0 ] = new Vector3();
-		var min = Number.MAX_VALUE;
-		var tx = Math.abs( tangents[ 0 ].x );
-		var ty = Math.abs( tangents[ 0 ].y );
-		var tz = Math.abs( tangents[ 0 ].z );
-
-		if ( tx <= min ) {
-
-			min = tx;
-			normal.set( 1, 0, 0 );
-
-		}
-
-		if ( ty <= min ) {
-
-			min = ty;
-			normal.set( 0, 1, 0 );
-
-		}
-
-		if ( tz <= min ) {
-
-			normal.set( 0, 0, 1 );
-
-		}
-
-		vec.crossVectors( tangents[ 0 ], normal ).normalize();
-
-		normals[ 0 ].crossVectors( tangents[ 0 ], vec );
-		binormals[ 0 ].crossVectors( tangents[ 0 ], normals[ 0 ] );
-
-
-		// compute the slowly-varying normal and binormal vectors for each segment on the curve
-
-		for ( i = 1; i <= segments; i ++ ) {
-
-			normals[ i ] = normals[ i - 1 ].clone();
-
-			binormals[ i ] = binormals[ i - 1 ].clone();
-
-			vec.crossVectors( tangents[ i - 1 ], tangents[ i ] );
-
-			if ( vec.length() > Number.EPSILON ) {
-
-				vec.normalize();
-
-				theta = Math.acos( _Math.clamp( tangents[ i - 1 ].dot( tangents[ i ] ), - 1, 1 ) ); // clamp for floating pt errors
-
-				normals[ i ].applyMatrix4( mat.makeRotationAxis( vec, theta ) );
-
-			}
-
-			binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
-
-		}
-
-		// if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
-
-		if ( closed === true ) {
-
-			theta = Math.acos( _Math.clamp( normals[ 0 ].dot( normals[ segments ] ), - 1, 1 ) );
-			theta /= segments;
-
-			if ( tangents[ 0 ].dot( vec.crossVectors( normals[ 0 ], normals[ segments ] ) ) > 0 ) {
-
-				theta = - theta;
-
-			}
-
-			for ( i = 1; i <= segments; i ++ ) {
-
-				// twist a little...
-				normals[ i ].applyMatrix4( mat.makeRotationAxis( tangents[ i ], theta * i ) );
-				binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
-
-			}
-
-		}
-
-		return {
-			tangents: tangents,
-			normals: normals,
-			binormals: binormals
-		};
-
-	},
-
-	clone: function () {
-
-		return new this.constructor().copy( this );
-
-	},
-
-	copy: function ( source ) {
-
-		this.arcLengthDivisions = source.arcLengthDivisions;
-
-		return this;
-
-	},
-
-	toJSON: function () {
-
-		var data = {
-			metadata: {
-				version: 4.5,
-				type: 'Curve',
-				generator: 'Curve.toJSON'
-			}
-		};
-
-		data.arcLengthDivisions = this.arcLengthDivisions;
-		data.type = this.type;
-
-		return data;
-
-	},
-
-	fromJSON: function ( json ) {
-
-		this.arcLengthDivisions = json.arcLengthDivisions;
-
-		return this;
-
-	}
-
-} );
-
+    var i, u, theta;
+
+    // compute the tangent vectors for each segment on the curve
+
+    for (i = 0; i <= segments; i++) {
+      u = i / segments;
+
+      tangents[i] = this.getTangentAt(u);
+      tangents[i].normalize();
+    }
+
+    // select an initial normal vector perpendicular to the first tangent vector,
+    // and in the direction of the minimum tangent xyz component
+
+    normals[0] = new Vector3();
+    binormals[0] = new Vector3();
+    var min = Number.MAX_VALUE;
+    var tx = Math.abs(tangents[0].x);
+    var ty = Math.abs(tangents[0].y);
+    var tz = Math.abs(tangents[0].z);
+
+    if (tx <= min) {
+      min = tx;
+      normal.set(1, 0, 0);
+    }
+
+    if (ty <= min) {
+      min = ty;
+      normal.set(0, 1, 0);
+    }
+
+    if (tz <= min) {
+      normal.set(0, 0, 1);
+    }
+
+    vec.crossVectors(tangents[0], normal).normalize();
+
+    normals[0].crossVectors(tangents[0], vec);
+    binormals[0].crossVectors(tangents[0], normals[0]);
+
+    // compute the slowly-varying normal and binormal vectors for each segment on the curve
+
+    for (i = 1; i <= segments; i++) {
+      normals[i] = normals[i - 1].clone();
+
+      binormals[i] = binormals[i - 1].clone();
+
+      vec.crossVectors(tangents[i - 1], tangents[i]);
+
+      if (vec.length() > Number.EPSILON) {
+        vec.normalize();
+
+        theta = Math.acos(_Math.clamp(tangents[i - 1].dot(tangents[i]), -1, 1)); // clamp for floating pt errors
+
+        normals[i].applyMatrix4(mat.makeRotationAxis(vec, theta));
+      }
+
+      binormals[i].crossVectors(tangents[i], normals[i]);
+    }
+
+    // if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
+
+    if (closed === true) {
+      theta = Math.acos(_Math.clamp(normals[0].dot(normals[segments]), -1, 1));
+      theta /= segments;
+
+      if (
+        tangents[0].dot(vec.crossVectors(normals[0], normals[segments])) > 0
+      ) {
+        theta = -theta;
+      }
+
+      for (i = 1; i <= segments; i++) {
+        // twist a little...
+        normals[i].applyMatrix4(mat.makeRotationAxis(tangents[i], theta * i));
+        binormals[i].crossVectors(tangents[i], normals[i]);
+      }
+    }
+
+    return {
+      tangents: tangents,
+      normals: normals,
+      binormals: binormals
+    };
+  },
+
+  clone: function() {
+    return new this.constructor().copy(this);
+  },
+
+  copy: function(source) {
+    this.arcLengthDivisions = source.arcLengthDivisions;
+
+    return this;
+  },
+
+  toJSON: function() {
+    var data = {
+      metadata: {
+        version: 4.5,
+        type: "Curve",
+        generator: "Curve.toJSON"
+      }
+    };
+
+    data.arcLengthDivisions = this.arcLengthDivisions;
+    data.type = this.type;
+
+    return data;
+  },
+
+  fromJSON: function(json) {
+    this.arcLengthDivisions = json.arcLengthDivisions;
+
+    return this;
+  }
+});
 
 export { Curve };
--- src/extras/core/Shape.js
+++ src/extras/core/Shape.js
@@ -1,10 +1,5 @@
-import { Path } from './Path.js';
-import { _Math } from '../../math/Math.js';
-
-/**
- * @author zz85 / http://www.lab4games.net/zz85/blog
- * Defines a 2d shape plane using paths.
- **/
+import { Path } from "./Path.js";
+import { _Math } from "../../math/Math.js";
 
 // STEP 1 Create a path.
 // STEP 2 Turn path into shape.
@@ -12,105 +7,88 @@ import { _Math } from '../../math/Math.js';
 // STEP 3a - Extract points from each shape, turn to vertices
 // STEP 3b - Triangulate each shape, add faces.
 
-// 利用这个类可以创造任意路径的图形
+//
+/**
+ * 利用这个类可以创造任意路径的图形
+ * @param {*} points 控制点数组
+ * （1）创建一个path路径曲线对象
+ * （2）将path路径曲线转换为shape对象
+ * （3）将shape对象“充气”变成三维模型。其中分两步，第一步提取每个shape对象的点数据并转换为顶点；第二步利用顶点进行三角化处理，从而形成面
+ */
 function Shape(points) {
+  Path.call(this, points);
 
-    Path.call(this, points);
+  this.uuid = _Math.generateUUID();
 
-    this.uuid = _Math.generateUUID();
+  this.type = "Shape";
 
-    this.type = 'Shape';
-
-    this.holes = [];
-
+  // shape对象可能存在镂空的形态；注意数组每个元素其实就是path实例
+  this.holes = [];
 }
 
 Shape.prototype = Object.assign(Object.create(Path.prototype), {
+  constructor: Shape,
 
-    constructor: Shape,
+  //   得到形成holes的path对象的采样点
+  getPointsHoles: function(divisions) {
+    var holesPts = [];
 
-    getPointsHoles: function(divisions) {
-
-        var holesPts = [];
-
-        for (var i = 0, l = this.holes.length; i < l; i++) {
-
-            holesPts[i] = this.holes[i].getPoints(divisions);
-
-        }
-
-        return holesPts;
-
-    },
-
-    // get points of shape and holes (keypoints based on segments parameter)
-
-    extractPoints: function(divisions) {
-
-        return {
-
-            shape: this.getPoints(divisions),
-            holes: this.getPointsHoles(divisions)
-
-        };
-
-    },
-
-    copy: function(source) {
-
-        Path.prototype.copy.call(this, source);
-
-        this.holes = [];
-
-        for (var i = 0, l = source.holes.length; i < l; i++) {
-
-            var hole = source.holes[i];
-
-            this.holes.push(hole.clone());
-
-        }
-
-        return this;
-
-    },
-
-    toJSON: function() {
-
-        var data = Path.prototype.toJSON.call(this);
-
-        data.uuid = this.uuid;
-        data.holes = [];
-
-        for (var i = 0, l = this.holes.length; i < l; i++) {
-
-            var hole = this.holes[i];
-            data.holes.push(hole.toJSON());
-
-        }
-
-        return data;
-
-    },
-
-    fromJSON: function(json) {
-
-        Path.prototype.fromJSON.call(this, json);
-
-        this.uuid = json.uuid;
-        this.holes = [];
-
-        for (var i = 0, l = json.holes.length; i < l; i++) {
-
-            var hole = json.holes[i];
-            this.holes.push(new Path().fromJSON(hole));
-
-        }
-
-        return this;
-
+    for (var i = 0, l = this.holes.length; i < l; i++) {
+      holesPts[i] = this.holes[i].getPoints(divisions);
     }
 
-});
+    return holesPts;
+  },
 
+  //   采样shape曲线点，以及shape中存在的漏洞曲线点
+  extractPoints: function(divisions) {
+    return {
+      shape: this.getPoints(divisions),
+      holes: this.getPointsHoles(divisions)
+    };
+  },
+
+  copy: function(source) {
+    Path.prototype.copy.call(this, source);
+
+    this.holes = [];
+
+    for (var i = 0, l = source.holes.length; i < l; i++) {
+      var hole = source.holes[i];
+
+      this.holes.push(hole.clone());
+    }
+
+    return this;
+  },
+
+  toJSON: function() {
+    var data = Path.prototype.toJSON.call(this);
+
+    data.uuid = this.uuid;
+    data.holes = [];
+
+    for (var i = 0, l = this.holes.length; i < l; i++) {
+      var hole = this.holes[i];
+      data.holes.push(hole.toJSON());
+    }
+
+    return data;
+  },
+
+  fromJSON: function(json) {
+    Path.prototype.fromJSON.call(this, json);
+
+    this.uuid = json.uuid;
+    this.holes = [];
+
+    for (var i = 0, l = json.holes.length; i < l; i++) {
+      var hole = json.holes[i];
+      this.holes.push(new Path().fromJSON(hole));
+    }
+
+    return this;
+  }
+});
 
 export { Shape };
